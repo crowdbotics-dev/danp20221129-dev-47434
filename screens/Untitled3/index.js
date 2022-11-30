8ccf5974e7c4065029ecddcdc07b2b6db1aede21
Daniel Palacios <daniel80380@gmail.com>
2022-11-29 21:12:11 -0300

    Crowdbotics Studio: Update app screens

--- screens/Untitled3/index.js
+++ screens/Untitled3/index.js
@@ -1,15 +1,15 @@
 import React from "react";
 import { View, StyleSheet } from "react-native";
 
-const Untitled1 = () => {
-  return <View style={_styles.AdtTBmRU}>
+const Untitled3 = () => {
+  return <View style={_styles.mnrnJxFd}>
       </View>;
 };
 
-export default Untitled1;
+export default Untitled3;
 
 const _styles = StyleSheet.create({
-  AdtTBmRU: {
+  mnrnJxFd: {
     backgroundColor: "#f0f0f1",
     padding: 10,
     position: "relative",
