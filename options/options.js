49acc0050c9950a0f20c1da39b23cac5e2906462
Daniel Palacios <daniel80380@gmail.com>
2022-11-29 21:11:47 -0300

    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,10 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
   name: "danp20221129-dev-47434",
   url: "https://danp20221129-dev-47434.botics.co",
   api: "https://danp20221129-dev-47434.botics.co/api/v1"
-}
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
